18c04a66db6356738568d3f77587c3e25edeb715
luoxiao <luoxiaozero@163.com>
2024-08-28 14:49:32 +0800

    feat: add ViewFoldingRangeProvider

---
+++ src/foldingProvider.ts
@@ -0,0 +1,29 @@
+import * as vscode from "vscode";
+
+export class ViewFoldingRangeProvider implements vscode.FoldingRangeProvider {
+    provideFoldingRanges(
+        document: vscode.TextDocument,
+        context: vscode.FoldingContext,
+        token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.FoldingRange[]> {
+        const foldingRanges: vscode.FoldingRange[] = [];
+        const text = document.getText();
+        const regex = /<div>([\s\S]*?)<\/div>/g;
+        // const viewMacroStartRegex = /view!\s*\{/g;
+
+        let match;
+        while ((match = regex.exec(text)) !== null) {
+            const start = document.positionAt(match.index);
+            const end = document.positionAt(match.index + match[0].length);
+            foldingRanges.push(
+                new vscode.FoldingRange(
+                    start.line,
+                    end.line,
+                    vscode.FoldingRangeKind.Region
+                )
+            );
+        }
+
+        return foldingRanges;
+    }
+}
