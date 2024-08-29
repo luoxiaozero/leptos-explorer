8615da5684988aeff9aff60c250722ccece71973
luoxiao <luoxiaozero@163.com>
2024-08-29 15:26:11 +0800

    feat: vsce package

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
             // The code you place here will be executed every time your command is executed
             // Display a message box to the user
             vscode.window.showInformationMessage(
-                "Hello World from Leptos Explore2!"
+                "Hello World from Leptos Explore!"
             );
         }
     );
